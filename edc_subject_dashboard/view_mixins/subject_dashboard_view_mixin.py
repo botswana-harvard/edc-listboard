8adfc228c6c39468ed9d041703b87612e5be5738
Coulson Kgathi <ckgathi@gmail.com>
2019-01-03 00:15:04 +0200

    cater for infant dashboard

--- edc_subject_dashboard/view_mixins/subject_dashboard_view_mixin.py
+++ edc_subject_dashboard/view_mixins/subject_dashboard_view_mixin.py
@@ -8,7 +8,6 @@ from edc_visit_schedule.view_mixins import VisitScheduleViewMixin
 from .registered_subject_view_mixin import RegisteredSubjectViewMixin
 from .subject_visit_view_mixin import SubjectVisitViewMixin
 from django.views.generic.base import ContextMixin
-from pprint import pprint
 
 
 class VerifyRequisitionMixin(ContextMixin):
@@ -17,6 +16,52 @@ class VerifyRequisitionMixin(ContextMixin):
         context = super().get_context_data(**kwargs)
         scanning = self.kwargs.get('scanning')
         context.update(scanning=scanning)
+        return context
+
+
+class SpecialForms(ContextMixin):
+
+    special_forms_include_value = None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(special_forms_include_value=self.special_forms_include_value)
+        return context
+
+
+class InfantDashboardLinks(ContextMixin):
+
+    mother_infant_study = False
+    infant_dashboard_include_value = None
+    infant_subject_dashboard_url = None
+
+    @property
+    def infant_registered_subject(self):
+        """Returns an infant registered subject.
+        """
+        return None
+
+    @property
+    def infant_birth(self):
+        """Returns and infant birth.
+        """
+        return None
+
+    @property
+    def wrapped_infant_birth(self):
+        """Return a wrapped infant birth instance.
+        """
+        return None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(
+            infant_subject_dashboard_url=self.infant_subject_dashboard_url,
+            infant_birth=self.infant_birth,
+            wrapped_infant_birth=self.wrapped_infant_birth,
+            infant_registered_subject=self.infant_registered_subject,
+            mother_infant_study=self.mother_infant_study,
+            infant_dashboard_include_value=self.infant_dashboard_include_value)
         return context
 
 
@@ -29,6 +74,8 @@ class SubjectDashboardViewMixin(
         SubjectVisitViewMixin,
         VisitScheduleViewMixin,
         RegisteredSubjectViewMixin,
-        VerifyRequisitionMixin):
+        VerifyRequisitionMixin,
+        SpecialForms,
+        InfantDashboardLinks):
 
     pass
