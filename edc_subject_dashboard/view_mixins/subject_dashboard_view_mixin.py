5ca34c7932983a7fb278c2b2ba8cfd03f0abb1ca
COulson Kgathi <ckgathi@gmail.com>
2020-04-22 00:59:01 +0200

    add data manager module

--- edc_subject_dashboard/view_mixins/subject_dashboard_view_mixin.py
+++ edc_subject_dashboard/view_mixins/subject_dashboard_view_mixin.py
@@ -1,13 +1,16 @@
+from django.views.generic.base import ContextMixin
+
 from edc_action_item.view_mixins import ActionItemViewMixin
 from edc_appointment.view_mixins import AppointmentViewMixin
 from edc_consent.view_mixins import ConsentViewMixin
+from edc_data_manager.view_mixins import DataActionItemsViewMixin
 from edc_locator.view_mixins import SubjectLocatorViewMixin
 from edc_metadata.view_mixins import MetaDataViewMixin
 from edc_visit_schedule.view_mixins import VisitScheduleViewMixin
 
 from .registered_subject_view_mixin import RegisteredSubjectViewMixin
 from .subject_visit_view_mixin import SubjectVisitViewMixin
-from django.views.generic.base import ContextMixin
+
 
 
 class VerifyRequisitionMixin(ContextMixin):
@@ -82,6 +85,7 @@ class SubjectDashboardViewMixin(
         RegisteredSubjectViewMixin,
         VerifyRequisitionMixin,
         SpecialForms,
+        DataActionItemsViewMixin,
         MaternalInfantDashboardLinks):
 
     pass
