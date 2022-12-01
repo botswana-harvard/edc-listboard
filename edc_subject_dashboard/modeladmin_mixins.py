448b9480c61573b1d8e9d917b360a7e0fbdd51b6
Moses Chawawa <moseschawawa@gmail.com>
2022-12-01 16:06:53 +0200

    URL exception fix

--- edc_subject_dashboard/modeladmin_mixins.py
+++ edc_subject_dashboard/modeladmin_mixins.py
@@ -1,6 +1,7 @@
 from django.urls.base import reverse
 from django.conf import settings
 from django.utils.safestring import mark_safe
+from django.urls.exceptions import NoReverseMatch
 
 
 class ModelAdminSubjectDashboardMixin:
@@ -8,9 +9,17 @@ class ModelAdminSubjectDashboardMixin:
     subject_dashboard_url = 'subject_dashboard_url'
 
     def dashboard(self, obj=None):
-        url = reverse(settings.DASHBOARD_URL_NAMES.get(self.subject_dashboard_url),
+
+        try:
+            
+            url = reverse(settings.DASHBOARD_URL_NAMES.get(self.subject_dashboard_url),
                       kwargs=dict(subject_identifier=obj.subject_identifier))
-        return mark_safe(
-            f'<a class="button" title="go to subject dashboard" '
-            f'href="{url}">{obj.subject_identifier}</a>')
+            
+        except NoReverseMatch:
+            pass
+        else:
+            return mark_safe(
+                f'<a class="button" title="go to subject dashboard" '
+                f'href="{url}">{obj.subject_identifier}</a>')
+            
     dashboard.short_description = 'Dashboard'
